56af0fb9aecb6ce6b7eb1a5f4e55ee5ed1ea1b3f
frankworks <frankfax@hotmail.com>
2017-09-22 01:00:41 -0700

    1st Commision

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloRaspbian
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Internet of Things!");
+        }
+    }
+}
